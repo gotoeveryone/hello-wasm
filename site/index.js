7139651901d458eabc61605d757cb7c4cbeb1aad
Kazuki_Kamizuru <k.amago0721@gmail.com>
2021-12-06 09:41:00 +0900

    feat: run alert when trigger event of button click

--- site/index.js
+++ site/index.js
@@ -1,4 +1,8 @@
 const js = import("./node_modules/@gotoeveryone/hello-wasm/hello_wasm.js");
-js.then(js => {
-  js.greet("WebAssembly");
+
+document.querySelector("#send").addEventListener("click", () => {
+  const name = document.querySelector("#name").value;
+  js.then(js => {
+    js.greet(name);
+  }); 
 });
